02fd63dbedba73b9a6d085be5e24a336bd487124
firas makni <firas makni>
2017-03-13 15:08:58 +0100

    merge fi ras refrech liste, dossier

--- src/services/RigimeService.ts
+++ src/services/RigimeService.ts
@@ -2,19 +2,20 @@ import {SQLite} from 'ionic-native';
 import {Rigime} from "../models/Rigime";
 
 export class RigimeService {
-  public rigime= new Rigime();
+  public rigime = new Rigime();
   verif: boolean;
 
   constructor() {
   }
-  public verifRigime(rigimes: any,numdoss, datefeuille, nature) {
+
+  public verifRigime(rigimes: any, numdoss, datefeuille, nature) {
     this.verif = false;
     let db = new SQLite();
     db.openDatabase({
       name: 'clinisys.db',
       location: 'default' // the location field is required
     }).then(() => {
-      db.executeSql("select * from Rigime where numdoss like '" + numdoss + "' and datefeuille like '"+datefeuille+"' and nature like '"+nature+"'", [])
+      db.executeSql("select * from Rigime where numdoss like '" + numdoss + "' and datefeuille like '" + datefeuille + "' and nature like '" + nature + "'", [])
         .then(result => {
           if (result.rows.length === rigimes.length) {
             this.verif = true;
@@ -29,20 +30,20 @@ export class RigimeService {
     return this.verif;
   }
 
- public getRigimes(rigimes,numdoss, datefeuille, nature) {
+  public getRigimes(rigimes, numdoss, datefeuille, nature) {
 
     let db = new SQLite();
     db.openDatabase({
       name: 'clinisys.db',
       location: 'default' // the location field is required
     }).then(() => {
-      db.executeSql("select * from Rigime where numdoss like '" + numdoss + "' and datefeuille like '"+datefeuille+"' and nature like '"+nature+"'", [])
-         .then(result => {
+      db.executeSql("select * from Rigime where numdoss like '" + numdoss + "' and datefeuille like '" + datefeuille + "' and nature like '" + nature + "'", [])
+        .then(result => {
           if (result.rows.length === 0) {
-            this._insertRigimes(rigimes,numdoss, datefeuille, nature);
+            this._insertRigimes(rigimes, numdoss, datefeuille, nature);
           } else {
-               this.rigime.setcodeRegime(result.rows.item(0).codeRegime);
-               this.rigime.setdesignation(result.rows.item(0).designation);
+            this.rigime.setcodeRegime(result.rows.item(0).codeRegime);
+            this.rigime.setdesignation(result.rows.item(0).designation);
 
           }
         })
@@ -55,20 +56,20 @@ export class RigimeService {
     return this.rigime;
   }
 
- private _insertRigimes(rigime,numdoss, datefeuille, nature): void {
+  private _insertRigimes(rigime, numdoss, datefeuille, nature): void {
     let db = new SQLite();
     db.openDatabase({
       name: 'clinisys.db',
       location: 'default' // the location field is required
     }).then(() => {
-        db.executeSql('insert into Rigime (codeRegime, designation ,numdoss ' +
-            ',datefeuille, nature) values (?,?,?,?,?)', [
-          rigime.getcodeRegime(),
-              rigime.getdesignation(),
-              numdoss,
-              datefeuille,
-              nature
-        ]);
+      db.executeSql('insert into Rigime (codeRegime, designation ,numdoss ' +
+        ',datefeuille, nature) values (?,?,?,?,?)', [
+        rigime.getcodeRegime(),
+        rigime.getdesignation(),
+        numdoss,
+        datefeuille,
+        nature
+      ]);
     }).catch(error => {
       console.error('Error opening database', error);
       alert('Error 2 Rigime ' + error);
@@ -77,7 +78,7 @@ export class RigimeService {
   }
 
 
-   public deleteRigimes(numdoss, datefeuille, nature) {
+  public deleteRigimes(numdoss, datefeuille, nature) {
 
     let db = new SQLite();
     db.openDatabase({
@@ -86,7 +87,7 @@ export class RigimeService {
     }).then(() => {
       db.executeSql("delete from Rigime where numdoss like '" + numdoss + "' and datefeuille like '" + datefeuille + "' and nature like '" + nature + "'", [])
         .then(() => {
-    //      alert("Suppression de table Rigime est terminé avec succes");
+          //      alert("Suppression de table Rigime est terminé avec succes");
         })
         .catch(error => {
           console.error('Error opening database', error);
